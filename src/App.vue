1564d1fca0367782c14c574348609405f9e58e25
Cristian Sena <csena@gett.mobi>
2024-07-20 19:39:56 -0300

    Agrego estilos al home

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,56 @@
 <script setup>
 
+
 </script>
 
 <template>
-  <h1>Control de Gastos</h1>
+  <div>
+    <header>
+      <h1>Planificador de Gastos</h1>
+    </header>
+  </div>
 </template>
 
-<style scoped>
+<style>
+  :root {
+    --azul: #3b82f6;
+    --blanco: #fff;
+    --gris: #94a3b8;
+    --gris-claro: #f5f5f5;
+    --gris-oscuro: #64748b;
+    --negro: #000;
+  }
+
+  html {
+    font-size: 62.5%;
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-size: 1.6rem;
+    font-family: 'Lato', sans-serif;
+    background-color: var(--gris-claro);
+  }
+
+  h1 {
+    font-size: 4rem;
+  }
+
+  h2 {
+    font-size: 3rem;
+  }
+
+header {
+    background-color: var(--azul);
+}
+header h1 {
+    color: var(--blanco);
+    margin: 0;
+    padding: 3rem 0;
+    text-align: center;
+}
 
 </style>
